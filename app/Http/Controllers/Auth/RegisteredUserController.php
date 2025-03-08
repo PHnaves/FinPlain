dbf6273abf5aa1bdf64d3358c343e9e4264ec36b
PHnaves <pnaves001@gmail.com>
2025-03-08 14:32:16 -0300

    Adição do typeUser(basico, moderado ou gastador)  no cadastro e login de usuario.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,12 +33,14 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'typeUser' => ['required', 'in:basico,moderado,gastador'],//validacao enum
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'typeUser' => $request->typeUser,
         ]);
 
         event(new Registered($user));
