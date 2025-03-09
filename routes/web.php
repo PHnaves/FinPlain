c11898352725c1180d62a9ed67200ba6145a0259
PHnaves <pnaves001@gmail.com>
2025-03-09 18:56:14 -0300

    Ajuste nas rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\DespesaController;
+use App\Http\Controllers\MetaController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RendaController;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('metas', MetaController::class);
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('renda', RendaController::class)->only(['index', 'store']);
+    Route::resource('despesa', DespesaController::class)->only(['index', 'store']);
+});
 
 Route::get('/', function () {
     return view('welcome');
