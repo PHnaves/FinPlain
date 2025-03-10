7fcadf7b41e47c0bed5eafb3d38c0f5d01fa9e42
PHnaves <pnaves001@gmail.com>
2025-03-10 13:52:28 -0300

    Rota dashboard reajustada

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DespesaController;
 use App\Http\Controllers\MetaController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RendaController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
 
 Route::middleware(['auth'])->group(function () {
     Route::resource('metas', MetaController::class);
@@ -19,9 +25,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
