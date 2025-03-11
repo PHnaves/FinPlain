8b6a61a36b67f392347f6741b138e61880f34c7c
PHnaves <pnaves001@gmail.com>
2025-03-11 09:05:25 -0300

    Adição do campo renda a migration de users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('typeUser')->enum('basico', 'moderado', 'gastador');
+            $table->decimal('renda', 10, 2);
             $table->rememberToken();
             $table->timestamps();
         });
