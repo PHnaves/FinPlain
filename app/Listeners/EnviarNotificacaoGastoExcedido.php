acc514accd1831bda5a09b14c31c833cfb2ed9e5
PHnaves <pnaves001@gmail.com>
2025-03-12 18:19:45 -0300

    event de gasto

---
+++ app/Listeners/EnviarNotificacaoGastoExcedido.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\GastoExcedido;
+use App\Models\Notificacao;
+use Illuminate\Support\Facades\Mail;
+
+class EnviarNotificacaoGastoExcedido
+{
+    public function handle(GastoExcedido $event)
+    {
+        $user = $event->user;
+        $gasto = $event->gasto;
+    
+        // Evitar duplicação verificando antes de inserir
+        $existeNotificacao = Notificacao::where('id_user', $user->id)
+            ->where('mensagem', "Atenção! Sua despesa '{$gasto->descricao}' ultrapassou 50% da sua renda!")
+            ->exists();
+    
+        if (!$existeNotificacao) {
+            Notificacao::create([
+                'id_user' => $user->id,
+                'mensagem' => "Atenção! Sua despesa '{$gasto->descricao}' ultrapassou 50% da sua renda!",
+            ]);
+        }
+    }
+    
+}
