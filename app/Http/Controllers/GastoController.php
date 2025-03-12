2542cd4cc7a62dd03dc3c9fa47814b4b18f62a36
PHnaves <pnaves001@gmail.com>
2025-03-12 12:24:42 -0300

    Metodos CRUD para gastos

--- app/Http/Controllers/GastoController.php
+++ app/Http/Controllers/GastoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Gasto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GastoController extends Controller
 {
@@ -12,39 +13,39 @@ class GastoController extends Controller
      */
     public function index()
     {
-        //
+        $gastos = Gasto::all();
+        return view('gastos.index', compact('gastos'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'descricao' => 'required|string|max:150',
+            'valor' => 'required|numeric|min:0',
+            'necessario' => 'required|in:sim,nao',
+        ]);
+
+        Gasto::create([
+            'id_user' => Auth::id(),
+            'descricao' => $request->descricao,
+            'valor' => $request->valor,
+            'necessario' => $request->necessario,
+        ]);
+
+        return redirect()->route('gastos.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Gasto $gasto)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
      */
     public function edit(Gasto $gasto)
     {
-        //
+        return view('gastos.edit', compact('gasto'));
     }
 
     /**
@@ -52,7 +53,15 @@ class GastoController extends Controller
      */
     public function update(Request $request, Gasto $gasto)
     {
-        //
+        $request->validate([
+            'descricao' => 'required|string|max:150',
+            'valor' => 'required|numeric|min:0',
+            'necessario' => 'required|in:sim,nao',
+        ]);
+
+        $data = $request->only(['descricao', 'valor', 'necessario']);
+        $gasto->update($data);
+        return redirect()->route('gastos.index');
     }
 
     /**
@@ -60,6 +69,7 @@ class GastoController extends Controller
      */
     public function destroy(Gasto $gasto)
     {
-        //
+        $gasto->delete();
+        return redirect()->route('gastos.index');
     }
 }
