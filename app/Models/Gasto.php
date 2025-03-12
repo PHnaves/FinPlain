868d6eca9f037a94cb636f37dbf28dfb73e86d3f
PHnaves <pnaves001@gmail.com>
2025-03-12 12:25:19 -0300

    Adição do fillable no model de gasto

--- app/Models/Gasto.php
+++ app/Models/Gasto.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Gasto extends Model
 {
-    //
+
+    use HasFactory;
+    
+    protected $fillable = [
+        'id_user',
+        'descricao',
+        'valor',
+        'necessario',
+    ];
+
+    // Relacionamento com o usuário
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
+
+
