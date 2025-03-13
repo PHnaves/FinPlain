20a0da0bc8857b5078d8d2438dfc00a875b92e96
PHnaves <pnaves001@gmail.com>
2025-03-13 11:13:48 -0300

    Adição da logica no boot de providers para mostrar a quantidade de notificações não lidas na navbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Notificacao;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('layouts.navigation', function ($view) {
+            $naoLidas = 0;
+    
+            if (Auth::check()) {
+                $naoLidas = Notificacao::where('id_user', Auth::id())->where('lida', false)->count();
+            }
+    
+            $view->with('naoLidas', $naoLidas);
+        });
     }
 }
