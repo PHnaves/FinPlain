8f81264de786667ac2f49cbbe9f036753cb53924
PHnaves <pnaves001@gmail.com>
2025-03-19 13:28:35 -0300

    Adição do metodo schedule para realizar mais eficientemente o agendamento dos emails

--- routes/console.php
+++ routes/console.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Jobs\EnviarEmailDepositoMeta;
+use App\Jobs\EnviarEmailVencimentoDespesa;
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule; // Importação correta
 
+// Agendamento correto no Laravel 11
+Schedule::call(fn () => dispatch(new EnviarEmailVencimentoDespesa()))->dailyAt('08:00');
+Schedule::call(fn () => dispatch(new EnviarEmailDepositoMeta()))->everyMinute(); // Teste
+
+// Comando Artisan padrão
 Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
