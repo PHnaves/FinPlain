ad530242e2f20db22a2d5e64051df0bcc6ea011d
PHnaves <pnaves001@gmail.com>
2025-03-22 12:30:25 -0300

    Schedules para agendamento de jobs

--- routes/console.php
+++ routes/console.php
@@ -1,15 +1,16 @@
 <?php
 
-use App\Jobs\EnviarEmailDepositoMeta;
-use App\Jobs\EnviarEmailVencimentoDespesa;
+use App\Jobs\MetaJob;
+use App\Jobs\DespesaJob;
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule; // Importação correta
 
-// Agendamento correto no Laravel 11
-Schedule::call(fn () => dispatch(new EnviarEmailVencimentoDespesa()))->dailyAt('08:00');
-Schedule::call(fn () => dispatch(new EnviarEmailDepositoMeta()))->everyMinute(); // Teste
+// Agendando Jobs corretamente
+Schedule::job(new MetaJob())->dailyAt('11:00');
+Schedule::job(new DespesaJob())->dailyAt('11:00');
+
 
 // Comando Artisan padrão
 Artisan::command('inspire', function () {
