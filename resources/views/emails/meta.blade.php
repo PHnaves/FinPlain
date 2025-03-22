431660463ac5a047eb1bb5e1a3204e2ae73e36f0
PHnaves <pnaves001@gmail.com>
2025-03-22 12:32:32 -0300

    Views simples para emails, somente temporarias

--- resources/views/emails/meta.blade.php
+++ resources/views/emails/meta.blade.php
@@ -1,14 +1,8 @@
-@component('mail::message')
 # Olá, {{ $user->name }}
 
-Não se esqueça de depositar **R$ {{ number_format($meta->valor, 2, ',', '.') }}** para sua meta **{{ $meta->nome }}**.
+Não se esqueça de depositar **R$ {{ number_format($meta->valor_final ?? 0, 2, ',', '.') }}** para sua meta **{{ $meta->nome }}**.
 
 Pequenos passos levam a grandes conquistas!
 
-@component('mail::button', ['url' => url('/metas')])
-Ver Metas
-@endcomponent
-
 Atenciosamente,  
 **Equipe FinPlan**
-@endcomponent
--- resources/views/emails/vencimento.blade.php
+++ resources/views/emails/vencimento.blade.php
@@ -1,14 +1,8 @@
-@component('mail::message')
 # Olá, {{ $user->name }}
 
-Sua despesa **{{ $despesa->nome }}** no valor de **R$ {{ number_format($despesa->valor, 2, ',', '.') }}** vence hoje!
+Sua despesa **{{ $despesa->nome }}** no valor de **R$ {{ number_format($despesa->valor ?? 0, 2, ',', '.') }}** vence hoje!
 
 Evite juros e pague antes do vencimento.
 
-@component('mail::button', ['url' => url('/despesas')])
-Ver Despesas
-@endcomponent
-
 Atenciosamente,  
 **Equipe FinPlan**
-@endcomponent
