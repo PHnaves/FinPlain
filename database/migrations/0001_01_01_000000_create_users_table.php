23d274ade3f95ff85c8e16e1ad85175f361debfc
PHnaves <pnaves001@gmail.com>
2025-03-25 21:38:29 -0300

    modificacao no nome do campo da tabela de tipo de usuario

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('typeUser')->enum('basico', 'moderado', 'gastador');
+            $table->string('typeUser')->enum('conservador', 'moderado', 'arrojado');
             $table->decimal('renda', 10, 2);
             $table->rememberToken();
             $table->timestamps();
