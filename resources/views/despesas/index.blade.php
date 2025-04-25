332ced8ed4354c65f88eb8c7994ace082ffb01e9
PHnaves <pnaves001@gmail.com>
2025-04-25 17:12:49 -0300

    Button com link para direcinar o usuario para a pagina de gerar relatorios

--- resources/views/despesas/index.blade.php
+++ resources/views/despesas/index.blade.php
@@ -13,6 +13,10 @@
 
                     <h1>Minhas Despesas</h1>
 
+                    <a href="{{ route('relatorio.index') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Ver Relatórios
+                    </a>
+
                     <form action="{{ route('despesas.store') }}" method="POST">
                         @include('despesas.components.form')
                     </form>
