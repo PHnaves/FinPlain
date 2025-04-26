eb8764c850ed592033f78742f4124d6b88c53a0c
PHnaves <pnaves001@gmail.com>
2025-04-26 20:13:32 -0300

    Reajuste na logica para não enviar notificações repetidas

--- app/Console/Commands/ExpenseLimitValue.php
+++ app/Console/Commands/ExpenseLimitValue.php
@@ -6,6 +6,7 @@ use App\Models\Expense;
 use App\Models\User;
 use App\Notifications\ExpenseLimitValueNotification;
 use Illuminate\Console\Command;
+use Carbon\Carbon;
 
 class ExpenseLimitValue extends Command
 {
@@ -15,6 +16,7 @@ class ExpenseLimitValue extends Command
     public function handle()
     {
         $users = User::all(); // Buscar todos os usuários
+        $notifiedExpenses = []; // Para armazenar as despesas já notificadas, para assim não precisar criar mais um campo no banco de dados
 
         foreach ($users as $user) {
             if ($user->rent) { // Garantir que tenha um valor de renda
@@ -23,8 +25,12 @@ class ExpenseLimitValue extends Command
                 $expenses = Expense::where('user_id', $user->id)->get();
 
                 foreach ($expenses as $expense) {
-                    if ($expense->expense_value > $limit) {
+                    if ($expense->expense_value > $limit && !in_array($expense->id, $notifiedExpenses)) {
+                        // Envia a notificação
                         $user->notify(new ExpenseLimitValueNotification($expense));
+
+                        // Marca a despesa como notificada no ciclo
+                        $notifiedExpenses[] = $expense->id;
                     }
                 }
             }
