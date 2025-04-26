9c3f45496ab60f4d8a19bb5003d2e2b6a45c93e0
PHnaves <pnaves001@gmail.com>
2025-04-26 18:49:39 -0300

    Adição das schedules para verificar a cada minuto se há alguma notificação para ser enviada

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule; // Importação correta
 
+Schedule::command('notificar:despesas')->everyMinute();
+Schedule::command('notificar:depositos')->everyMinute();
+Schedule::command('despesas:limite')->everyMinute();
+
 // Agendando os Jobs corretamente para envio de e-mails
 Schedule::job(new GoalJob())->dailyAt('11:00'); // Agendando o GoalJob
 Schedule::job(new ExpenseJob())->dailyAt('11:00'); // Agendando o ExpenseJob
