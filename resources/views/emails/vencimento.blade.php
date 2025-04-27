cdbff28dd1edabd1da9a40cde9e4b77f56433df8
PHnaves <pnaves001@gmail.com>
2025-04-27 11:54:39 -0300

    View simples e temporaria para emails de despesas

--- resources/views/emails/vencimento.blade.php
+++ resources/views/emails/vencimento.blade.php
@@ -1,6 +1,6 @@
 # Olá, {{ $user->name }}
 
-Sua despesa **{{ $despesa->nome }}** no valor de **R$ {{ number_format($despesa->valor ?? 0, 2, ',', '.') }}** vence hoje!
+Sua despesa **{{ $expense->expense_name }}** no valor de **R$ {{ number_format($expense->expense_value ?? 0, 2, ',', '.') }}** vence hoje!
 
 Evite juros e pague antes do vencimento.
 
