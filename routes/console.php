11a34a66863b4457bb57dd9ed48a6c98685cfcf7
PHnaves <pnaves001@gmail.com>
2025-04-27 12:08:11 -0300

    Mudança no metodo schedule new job, pois estava errado, e mudança no horario de execução para todo dia as 6 da manhã

--- routes/console.php
+++ routes/console.php
@@ -7,13 +7,14 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule; // Importação correta
 
+// Agendando os comandos de notificações
 Schedule::command('notificar:despesas')->dailyAt('06:00');
 Schedule::command('notificar:depositos')->dailyAt('06:00');
-Schedule::command('despesas:limite')->everyMinute();
+Schedule::command('despesas:limite')->dailyAt('06:00');
 
 // Agendando os Jobs corretamente para envio de e-mails
-Schedule::job(new GoalJob())->dailyAt('11:00'); // Agendando o GoalJob
-Schedule::job(new ExpenseJob())->dailyAt('11:00'); // Agendando o ExpenseJob
+Schedule::job(new GoalJob)->dailyAt('06:00'); // Agendando o GoalJob
+Schedule::job(new ExpenseJob)->dailyAt('06:00'); // Agendando o ExpenseJob
 
 // Comando Artisan padrão
 Artisan::command('inspire', function () {
