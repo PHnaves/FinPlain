5018243e88f66e91bf4b941d620f63ad0dbf4dec
PHnaves <pnaves001@gmail.com>
2025-04-27 12:06:51 -0300

    Reajustado para atender aos novos arquivos

--- resources/views/emails/meta.blade.php
+++ resources/views/emails/meta.blade.php
@@ -1,6 +1,6 @@
 # Olá, {{ $user->name }}
 
-Não se esqueça de depositar **R$ {{ number_format($meta->valor_final ?? 0, 2, ',', '.') }}** para sua meta **{{ $meta->nome }}**.
+Não se esqueça de depositar **R$ {{ number_format($goal->target_value ?? 0, 2, ',', '.') }}** para sua meta **{{ $goal->goal_name }}**.
 
 Pequenos passos levam a grandes conquistas!
 
