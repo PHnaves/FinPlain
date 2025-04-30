0ae4aead4dc71fbb13e40fa07e50a2152916904e
PHnaves <pnaves001@gmail.com>
2025-04-29 21:34:59 -0300

    Adição de evento para carregar a classe 'loaded' no body: implementado um listener que adiciona a classe ao body quando a página termina de carregar, melhorando a experiência do usuário.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,10 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Adiciona a classe 'loaded' ao body quando a página terminar de carregar
+    window.addEventListener('load', () => {
+        document.body.classList.add('loaded');
+    });
+});
