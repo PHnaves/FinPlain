7cd61927ff8278bf640e319bafa7244a47356e68
PHnaves <pnaves001@gmail.com>
2025-05-05 14:36:10 -0300

    Começo do navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,138 +1,58 @@
-<nav x-data="{ open: false }" class="bg-primary-1 border-b border-primary-1">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-2 sm:px-2 lg:px-2">
-        <div class="flex justify-between h-16 pt-10">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center pb-3">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block fill-current text-white" />
-                    </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('despesas.index')" :active="request()->routeIs('despesas.index')">
-                        {{ __('Despesas') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('metas.index')" :active="request()->routeIs('metas.index')">
-                        {{ __('Metas') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('investiments.index')" :active="request()->routeIs('investiments.index')">
-                        {{ __('Investimentos') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('notifications')" :active="request()->routeIs('notifications')">
-                        {{ __('Notificações') }}
-                        @if(auth()->user()->unreadNotifications->count() > 0)
-                            <span class="badge bg-danger">{{ auth()->user()->unreadNotifications->count() }}</span>
-                        @endif
-                    </x-nav-link>
-                </div>
-            </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+    <!-- Navbar Desktop -->
+    <nav class="hidden sm:flex justify-between items-center p-2 bg-primary-1 text-white border-b border-primary-1 fixed w-full h-16 z-10">
+        <div class="flex items-center space-x-4">
+            <img src="{{ asset('/images/Logo-FinPlain.png')}}" class="h-12 w-12" alt="Logo">
+        </div>
+        <div class="flex items-center space-x-4">
+            <a href="{{route('notifications')}}" class="p-2 flex items-center hover:bg-primary-2 rounded-md">
+                <img src="{{ asset('/icons/bell.png') }}" class="h-5 w-auto" alt="Notificacao">
+                <span>
+                    @if(auth()->user()->unreadNotifications->count() > 0)
+                        <span class="text-red text-xs font-bold px-2 py-0.5 rounded-full">
+                            {{ auth()->user()->unreadNotifications->count() }}
+                        </span>
+                    @endif
+                </span>
+            </a>
+            <!-- Usuário (Dropdown) -->
+            <div class="mt-auto">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 pb-3 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-primary-1 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
+                        <button class="flex items-center justify-between w-full text-left px-4 py-2 hover:bg-primary-2 rounded-md">
+                            <span>{{ Auth::user()->name }}</span>
+                            <svg class="h-4 w-4 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                            </svg>
                         </button>
                     </x-slot>
-
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
-
-                        <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
                 </x-dropdown>
             </div>
-
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden pr-3">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
         </div>
-    </div>
+    </nav>
 
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-5 space-y-1 text-white">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('despesas.index')" :active="request()->routeIs('despesas.index')">
-                {{ __('Despesas') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('metas.index')" :active="request()->routeIs('metas.index')">
-                {{ __('Metas') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('investiments.index')" :active="request()->routeIs('investiments.index')">
-                {{ __('Investimentos') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('notifications')" :active="request()->routeIs('notifications')">
-                {{ __('Notificações') }}
-                @if(auth()->user()->unreadNotifications->count() > 0)
-                    <span class="badge bg-danger">{{ auth()->user()->unreadNotifications->count() }}</span>
-                @endif
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-white ">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-white">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
-        </div>
-    </div>
-</nav>
+    <!-- Sidebar Desktop -->
+    <aside class="fixed top-16 left-0 hidden sm:flex flex-col w-64 bg-primary-1 text-white border-r border-primary-2 h-full pt-8 space-y-2">
+        <a href="{{ route('dashboard') }}" class="flex items-center space-x-3 hover:bg-primary-2 px-4 py-2 rounded-md {{ request()->routeIs('dashboard') ? 'bg-primary-2' : '' }}">
+        <img src="{{asset('/icons/home.png')}}" alt="dashboard">
+        </a>
+        <a href="{{ route('despesas.index') }}" class="flex items-center space-x-3 hover:bg-primary-2 px-4 py-2 rounded-md {{ request()->routeIs('despesas.index') ? 'bg-primary-2' : '' }}">
+            <img src="{{asset('/icons/credit-card.png')}}" alt="despesa">
+        </a>
+        <a href="{{ route('metas.index') }}" class="flex items-center space-x-3 hover:bg-primary-2 px-4 py-2 rounded-md {{ request()->routeIs('metas.index') ? 'bg-primary-2' : '' }}">
+            <img src="{{asset('/icons/check-square.png')}}" alt="meta">
+        </a>
+        <a href="{{ route('investiments.index') }}" class="flex items-center space-x-3 hover:bg-primary-2 px-4 py-2 rounded-md {{ request()->routeIs('investiments.index') ? 'bg-primary-2' : '' }}">
+            <img src="{{asset('/icons/dollar-sign.png')}}" alt="investimentos">
+        </a>
+    </aside>
