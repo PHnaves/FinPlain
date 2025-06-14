fe571868a5a5bd3c460b68d67d7fc07385855a6d
PHnaves <pnaves001@gmail.com>
2025-06-14 19:03:24 -0300

    Adiciona nova imagem para a seção de dicas nas páginas de criação e edição de metas, além de atualizar o layout dos cards de publicidade, melhorando a estética e a responsividade.

--- resources/views/metas/edit.blade.php
+++ resources/views/metas/edit.blade.php
@@ -2,17 +2,6 @@
 
     <div class="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
-            @if (session('success'))
-                <div class="mb-6 p-4 bg-green-100 border border-green-400 text-green-700 rounded-lg">
-                    {{ session('success') }}
-                </div>
-            @endif
-
-            @if (session('error'))
-                <div class="mb-6 p-4 bg-red-100 border border-red-400 text-red-700 rounded-lg">
-                    {{ session('error') }}
-                </div>
-            @endif
             <!-- Formulário -->
             <div class="lg:col-span-2 bg-white shadow-md rounded-lg p-6">
                 <h2 class="text-3xl font-bold text-primary-1 p-4 md:p-6 border-b-2 border-primary-1 shadow-sm text-center">
@@ -26,7 +15,7 @@
             </div>
 
             <!-- Card de publicidade -->
-            <div class="h-[400px] bg-gradient-to-br from-emerald-500 to-emerald-700 text-white rounded-lg shadow-md p-4 flex flex-col justify-between">
+            <div class="h-[500px] md:h-[600px] bg-gradient-to-br from-indigo-500 to-indigo-700  text-white rounded-lg shadow-md p-4 flex flex-col justify-between">
                 <div>
                     <h3 class="text-xl font-semibold mb-3">Dicas para suas metas</h3>
                     <ul class="space-y-2 text-sm text-emerald-100">
@@ -51,7 +40,7 @@
                     </ul>
                 </div>
                 <div class="mt-4">
-                    <img src="https://cdn-icons-png.flaticon.com/512/2382/2382533.png" alt="Dicas" class="w-20 mx-auto">
+                    <img src="{{ asset('/images/Metas-removebg.png') }}" alt="Dicas" class="w-full h-auto max-h-[300px] object-contain">
                 </div>
             </div>
         </div>
