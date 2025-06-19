cf8691eedba0df4659dd798a17c060fa7cfab546
PHnaves <pnaves001@gmail.com>
2025-06-19 16:34:22 -0300

    Mudança nas mensagens de erro, estavão em ingles e foram passadas para o portugues.

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -32,6 +32,15 @@ class LoginRequest extends FormRequest
         ];
     }
 
+    public function messages() : array
+    {
+        return [
+            'email.required' => 'O email é obrigatorio',
+            'password.required' => 'A senha é obrigatoria',
+
+        ];
+    }
+
     /**
      * Attempt to authenticate the request's credentials.
      *
@@ -45,7 +54,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'email' => 'As credenciais fornecidas estão incorretas.',
             ]);
         }
 
