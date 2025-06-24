c26b11b16f71648ddc3b553b286719356c054976
PHnaves <pnaves001@gmail.com>
2025-06-24 19:45:56 -0300

    Adicionado rota de contato e tradução das rotas para o portugues.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DepositController;
 use App\Http\Controllers\ExpenseController;
@@ -13,27 +14,27 @@ use App\Http\Controllers\EarningController;
 
 // DEPOSITOS
 Route::middleware(['auth'])->group(function () {
-    Route::post('/deposit/{expense}', [DepositController::class, 'payExpense'])->name('pay.expense');
-    Route::post('/deposit/{goal}/deposit', [DepositController::class, 'depositGoal'])->name('deposit.goal');
+    Route::post('/deposito/{expense}', [DepositController::class, 'payExpense'])->name('pay.expense');
+    Route::post('/deposito/{goal}', [DepositController::class, 'depositGoal'])->name('deposit.goal');
 });
 
 //NOTIFICACOES
 Route::middleware('auth')->group(function () {
-    Route::get('/notifications', [NotificationsController::class, 'index'])->name('notifications');
-    Route::post('/notifications/{id}/markAsRead', [NotificationsController::class, 'markAsRead'])->name('notifications.markAsRead');
-    Route::delete('/notifications/{id}', [NotificationsController::class, 'destroy'])->name('notifications.destroy');
+    Route::get('/notificacoes', [NotificationsController::class, 'index'])->name('notifications');
+    Route::post('/notificacoes/{id}/lida', [NotificationsController::class, 'markAsRead'])->name('notifications.markAsRead');
+    Route::delete('/notificacoes/{id}', [NotificationsController::class, 'destroy'])->name('notifications.destroy');
 });
 
 //RELATORIOS
 Route::middleware(['auth'])->group(function () {
-    Route::get('/record', [RecordController::class, 'index'])->name('records.index');
-    Route::get('/record/generate', [RecordController::class, 'generatePdf'])->name('records.generate');
-    Route::post('/record/filter', [RecordController::class, 'filterExpenses'])->name('records.filter');
+    Route::get('/relatorio', [RecordController::class, 'index'])->name('records.index');
+    Route::get('/relatorio/gerar', [RecordController::class, 'generatePdf'])->name('records.generate');
+    Route::post('/relatorio/filtrar', [RecordController::class, 'filterExpenses'])->name('records.filter');
 });
 
 // INVESTIMENTOS
 Route::middleware(['auth'])->group(function () {
-    Route::get('/investiments', [InvestimentController::class, 'index'])->name('investiments.index');
+    Route::get('/investimentos', [InvestimentController::class, 'index'])->name('investiments.index');
 });
 
 // DASHBOARD
@@ -43,41 +44,43 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 // METAS
 Route::middleware(['auth'])->group(function () {
-    Route::get('/goal', [GoalController::class, 'index'])->name('metas.index'); // Listar metas
-    Route::get('/goal/create', [GoalController::class, 'create'])->name('metas.create');
-    Route::post('/goal', [GoalController::class, 'store'])->name('metas.store'); // Criar meta
-    Route::get('/goal/{goal}/show', [GoalController::class, 'show'])->name('metas.show'); // Ver detalhes da meta
-    Route::get('/goal/{goal}/edit', [GoalController::class, 'edit'])->name('metas.edit'); // Ver detalhes da meta
-    Route::patch('/goal/{goal}', [GoalController::class, 'update'])->name('metas.update'); // Atualizar meta
-    Route::delete('/goal/{goal}', [GoalController::class, 'destroy'])->name('metas.destroy'); // Deletar meta
+    Route::get('/meta', [GoalController::class, 'index'])->name('metas.index'); // Listar metas
+    Route::get('/meta/criar', [GoalController::class, 'create'])->name('metas.create');
+    Route::post('/meta', [GoalController::class, 'store'])->name('metas.store'); // Criar meta
+    Route::get('/meta/{goal}/detalhes', [GoalController::class, 'show'])->name('metas.show'); // Ver detalhes da meta
+    Route::get('/meta/{goal}/editar', [GoalController::class, 'edit'])->name('metas.edit'); // Ver detalhes da meta
+    Route::patch('/meta/{goal}', [GoalController::class, 'update'])->name('metas.update'); // Atualizar meta
+    Route::delete('/meta/{goal}', [GoalController::class, 'destroy'])->name('metas.destroy'); // Deletar meta
 });
 
 // DESPESAS
 Route::middleware(['auth'])->group(function () {
-    Route::get('/expense', [ExpenseController::class, 'index'])->name('despesas.index'); // Listar despesas
-    Route::get('/expense/create', [ExpenseController::class, 'create'])->name('despesas.create'); // Formulário de criação
-    Route::post('/expense', [ExpenseController::class, 'store'])->name('despesas.store'); // Criar despesa
-    Route::get('/expense/{expense}/show', [ExpenseController::class, 'show'])->name('despesas.show'); // Ver detalhes da despesa
-    Route::get('/expense/{expense}/edit', [ExpenseController::class, 'edit'])->name('despesas.edit'); // Ver detalhes da despesa
-    Route::patch('/expense/{expense}', [ExpenseController::class, 'update'])->name('despesas.update'); // Atualizar despesa
-    Route::delete('/expense/{expense}', [ExpenseController::class, 'destroy'])->name('despesas.destroy'); // Deletar despesa
+    Route::get('/despesa', [ExpenseController::class, 'index'])->name('despesas.index'); // Listar despesas
+    Route::get('/despesa/criar', [ExpenseController::class, 'create'])->name('despesas.create'); // Formulário de criação
+    Route::post('/despesa', [ExpenseController::class, 'store'])->name('despesas.store'); // Criar despesa
+    Route::get('/despesa/{expense}/detalhes', [ExpenseController::class, 'show'])->name('despesas.show'); // Ver detalhes da despesa
+    Route::get('/despesa/{expense}/editar', [ExpenseController::class, 'edit'])->name('despesas.edit'); // Ver detalhes da despesa
+    Route::patch('/despesa/{expense}', [ExpenseController::class, 'update'])->name('despesas.update'); // Atualizar despesa
+    Route::delete('/despesa/{expense}', [ExpenseController::class, 'destroy'])->name('despesas.destroy'); // Deletar despesa
 });
 
 // GANHOS
 Route::middleware(['auth'])->group(function () {
-    Route::get('/earning', [EarningController::class, 'index'])->name('ganhos.index'); // Listar ganhos
-    Route::get('/earning/create', [EarningController::class, 'create'])->name('ganhos.create'); // Formulário de criação
-    Route::post('/earning', [EarningController::class, 'store'])->name('ganhos.store'); // Criar ganho
-    Route::get('/earning/{earning}/show', [EarningController::class, 'show'])->name('ganhos.show'); // Ver detalhes do ganho
-    Route::get('/earning/{earning}/edit', [EarningController::class, 'edit'])->name('ganhos.edit'); // Editar ganho
-    Route::patch('/earning/{earning}', [EarningController::class, 'update'])->name('ganhos.update'); // Atualizar ganho
-    Route::delete('/earning/{earning}', [EarningController::class, 'destroy'])->name('ganhos.destroy'); // Deletar ganho
+    Route::get('/ganho', [EarningController::class, 'index'])->name('ganhos.index'); // Listar ganhos
+    Route::get('/ganho/criar', [EarningController::class, 'create'])->name('ganhos.create'); // Formulário de criação
+    Route::post('/ganho', [EarningController::class, 'store'])->name('ganhos.store'); // Criar ganho
+    Route::get('/ganho/{earning}/detalhes', [EarningController::class, 'show'])->name('ganhos.show'); // Ver detalhes do ganho
+    Route::get('/ganho/{earning}/editar', [EarningController::class, 'edit'])->name('ganhos.edit'); // Editar ganho
+    Route::patch('/ganho/{earning}', [EarningController::class, 'update'])->name('ganhos.update'); // Atualizar ganho
+    Route::delete('/ganho/{earning}', [EarningController::class, 'destroy'])->name('ganhos.destroy'); // Deletar ganho
 });
 
 //PAGINA BOAS VINDAS
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/contato', [ContactController::class, 'store'])->name('contato.store');
 
 //TERMOS E POLITICA DE PRIVACIDADE
 Route::get('/termos-de-uso', function () {
@@ -94,9 +97,9 @@ Route::get('/politica-de-privacidade', function () {
 
 //PERFIL
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/perfil', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/perfil', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/perfil', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
